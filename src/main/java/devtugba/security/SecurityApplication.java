23fd939c12ed7131082ad9f223cb9ebbb410271e
tugba-koc <tugba.koc@sisal.com>
2023-12-17 15:42:44 +0300

    feat: add transactionManagement annotation

--- src/main/java/devtugba/security/SecurityApplication.java
+++ src/main/java/devtugba/security/SecurityApplication.java
@@ -2,8 +2,10 @@ package devtugba.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SecurityApplication {
 
 	public static void main(String[] args) {
